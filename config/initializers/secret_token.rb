7c7cf5aca6290b6d106b9738ed6e9520c24eeaf9
Peter Tseng <patseng@stanford.edu>
2012-11-10 22:25:52 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventBuzz2::Application.config.secret_token = '7c6566607fc9877a58e279ef46539d94bd917c03692520c5ba5f2a3f706ab884a6f6bd42e55e684db2297fb6d600f162fa8fdd99219db0a9dbaff970e9a16533'
